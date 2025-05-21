53889d94b1f1cd14f7845508d14fd62d56cf96d4
picupiee <picupiee.id@gmail.com>
2025-05-21 08:47:46 +0700

    signin function and AuthProvider context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ export default function RootLayout() {
   return (
     <AuthProvider>
       <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="sign-in" options={{ headerShown: false }} />
         <Stack.Screen name="(app)" options={{ headerShown: false }} />
       </Stack>
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, TextInput, Button, Pressable } from "react-native";
 import { useAuth } from "./context/AuthContext";
 import { useRouter } from "expo-router";
 
@@ -13,4 +13,34 @@ export default function SignInScreen() {
     const fakeUserToken = "dummy-user";
     signIn(fakeUserToken);
   };
+
+  return (
+    <View className="flex-1 items-center justify-center">
+      <View className="mb-4">
+        <Text className="font-semibold text-lg underline underline-offset-4">
+          Sign In
+        </Text>
+      </View>
+      <View className="gap-2">
+        <TextInput
+          placeholder="Username"
+          onChangeText={setUsername}
+          autoCapitalize="none"
+          className="p-2 rounded-md bg-slate-200 outline-none"
+        />
+        <TextInput
+          placeholder="Password"
+          onChangeText={setPassword}
+          autoCapitalize="none"
+          className="p-2 rounded-md bg-slate-200 outline-none"
+        />
+      </View>
+      <Pressable
+        onPress={handleSignin}
+        className="mt-2 p-2 rounded-md bg-blue-500"
+      >
+        <Text>Sign In</Text>
+      </Pressable>
+    </View>
+  );
 }
